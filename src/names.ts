454fb079e943e79a12df3d350dff9d8969c8c5c7
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2021-10-15 11:57:10 +0200

    Revert "Extract commands from being inlined"
    This reverts commit 718460bcc6640c1618c9712e7b3bf7bb3b4384a6.
    
    This broke several of the commands in a way that cannot easily be fixed
    without changing parts of the architecture.

--- src/names.ts
+++
@@ -1,8 +0,0 @@
-/* Copyright (c) 2021 Nordic Semiconductor ASA
- *
- * SPDX-License-Identifier: LicenseRef-Nordic-1-Clause
- */
-
-export function toCIdentifier(name: string) {
-    return name.toLowerCase().replace(/[@,-]/g, '_').replace(/[#&]/g, '');
-}
